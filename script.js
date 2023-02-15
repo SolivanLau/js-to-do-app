7c1921aa4a328582e1a449468aa8900d592fb355
Solivan Lau <solivan_lau@outlook.com>
2023-02-15 15:45:23 -0500

    added pseudo code and logical comments

---
+++ script.js
@@ -0,0 +1,29 @@
+// Pseudo Code:
+// GOAL: user's string value will be added as an individual item to our to do list
+// TRACKING: submit event in our form input
+
+// 1. Save values:
+
+    // query dom for input element and save it in a variable
+    // query dom for form element and save it in a variable
+    // query dom for ul element and save it in a variable
+
+
+// 2. attach submit event listener to form element
+
+    // event listener method accepts two arguments
+    // event we are listening for
+    // callback function which holds logic we want to run
+
+    
+    // add the user's text content as a variable
+
+    //IF the user's value has content
+        // create an li
+        // add FA icon to li
+        // add user textcontent to the li element
+
+    
+        // append li element as a CHILD to the ul element
+
+    // ELSE alert the user to please submit a valid to do
